07e0c8bb4683e1a9663abf45b0390254d7d657f6
himarnoel <olaimarnoel@gmail.com>
2022-11-13 20:34:34 +0100

    working hero"

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,16 @@ import React from "react";
 const Hero = () => {
   return (
     <div>
-      <div className="w-full grid grid-cols-2 bg-red-500 text-black">
-        fdgsgdf
+      <div className="w-full grid grid-cols-2 bg-red-500 text-black mt-20">
+        <div>
+          <p>Rent a Place away from </p>
+          <p>Home in the Metaverse</p>
+          <p>
+            we provide you access to luxury and affordable houses in the
+            metaverse, get a chance to turn your imagination to reality at your
+            comfort zone
+          </p>
+        </div>
       </div>
     </div>
   );
