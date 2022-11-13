a80b7caba120667cc47b66a64640fc9274b0f543
himarnoel <olaimarnoel@gmail.com>
2022-11-13 23:30:38 +0100

    working on responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,39 +7,48 @@ const Hero = () => {
   return (
     <div>
       <div className="w-full grid lg:grid-cols-2  text-black mt-20  justify-between">
-        <div className=" p-10">
-          <p className="text-5xl mb-3">
+        <div className="p-10">
+          <p className="text-5xl mb-3 text-center">
             Rent a <a className="text-[#A02279]">Place</a> away from
             <br />
             <a className="text-[#A02279]">Home in the Metaverse</a>
           </p>
-          <p className="pt-10 text-3xl ">
+          <p className="lg:pt-10 lg:text-3xl text-center text-2xl  ">
             we provide you access to luxury and affordable houses in the
             metaverse, get a chance to turn your imagination to reality at your
             comfort zone
           </p>
-          <div class="input-group relative flex ">
-            <input
+          <div class="flex mt-20 ">
+          <input
               type="search"
-              class="form-control  block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white  border border-solid border-gray-300 rounded "
-              placeholder="Search"
+              className=" rounded-l-lg lg:px-20 lg:py-2.5   py-1.2 text-base font-normal text-gray-700 bg-white  border border-solid border-gray-300 "
+              placeholder="Search for location"
               aria-label="Search"
             />
             <button
-              class="btn inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"
+              className="btn lg:px-20 lg:py-2.5  px-1.5 py-2 rounded-r-lg bg-[#A02279] text-white font-medium text-xs  shadow-md "
               type="button"
-              id="button-addon2"
-            ></button>
+            >
+              Search
+            </button>
           </div>
         </div>
-        <div className="flex flex-row items-center  ml-56">
-          <div className="flex flex-col mr-5">
-            <img src={img1} alt="" className="object-contain w-60 mb-3" />
-            <img src={img2} alt="" className="object-contain w-60" />
+        <div className="flex flex-row items-center  lg:ml-56 mx-auto">
+          <div className="flex flex-col mr-5 mt-28">
+            <img
+              src={img1}
+              alt=""
+              className="object-contain w-32 lg:w-60 mb-3"
+            />
+            <img src={img4} alt="" className="object-contain w-32 lg:w-60" />
           </div>
-          <div className="flex flex-col mt-28">
-            <img src={img3} alt="" className="object-contain w-60 mb-3" />
-            <img src={img4} alt="" className="object-contain w-60" />
+          <div className="flex flex-col ">
+            <img
+              src={img3}
+              alt=""
+              className="object-contain w-32 lg:w-60 mb-3"
+            />
+            <img src={img2} alt="" className="object-contain w-32 lg:w-60" />
           </div>
         </div>
       </div>
