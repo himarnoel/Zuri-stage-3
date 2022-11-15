dc6f86f5bf9a1d565b41d3b26b72eea70984d59c
himarnoel <olaimarnoel@gmail.com>
2022-11-15 18:34:59 +0100

    still working on the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import num1 from "../assets/cards/1.png";
 const Card = () => {
   return (
-    <div>Card</div>
-  )
-}
+    <div>
+      <div className=" border-4 p-3 w-72 mb-20 mx-auto mt-10  rounded-xl">
+        <img src={num1} alt="" className="object-contain " />
+        <div className="flex justify-between">
+          <p>Desert King</p>
+          <p>1MBT per Night</p>
+        </div>
+        <div className="flex justify-between">
+          <p>2345km away</p>
+          <p>available for 2weeks stay</p>
+        </div>
+        <div className="text-left">
+            
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
