20f474d490d4e830ecc75c9b5904802cbaf44407
himarnoel <olaimarnoel@gmail.com>
2022-11-15 18:19:42 +0100

    added images for the card

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import Tape from "./components/Tape";
+import Card from "./components/Card";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <div className="text-center lg:text-xl xl:text-4xl text-xl">
         Inspiration for your next adventure
       </div>
+      <Card />
     </div>
   );
 }
--- src/components/Tape.jsx
+++ src/components/Tape.jsx
@@ -4,7 +4,7 @@ import num2 from "../assets/Group 59537.svg";
 import num3 from "../assets/Group 4040.svg";
 const Tape = () => {
   return (
-    <div className="flex justify-around w-full bg-[#A02279] p-3 items-center lg:mb-10 mt-3 lg:mt-0">
+    <div className="flex justify-between lg:px-28 py-2 w-full bg-[#A02279]  items-center lg:mb-10 mt-3 lg:mt-0">
       <img src={num1} alt="" className="object-contain w-24  lg:w-40" />
       <img src={num2} alt="" className="object-contain w-24 lg:w-40" />
       <img src={num3} alt="" className="object-contain w-24 lg:w-40" />
