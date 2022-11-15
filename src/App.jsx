dcbafe498e7cba980dbfc0c66e31ca80fe41e471
himarnoel <olaimarnoel@gmail.com>
2022-11-15 19:53:01 +0100

    purple side

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import Tape from "./components/Tape";
 import Card from "./components/Card";
-import Display from './components/Display';
+import Display from "./components/Display";
+import Nft from "./components/Nft";
 
 function App() {
   return (
     <div>
-      <div className="lg:px-14 xl:px-28">
         <Nav />
+      <div className="lg:px-14 xl:px-28">
         <Hero />
       </div>
       <Tape />
       <div className="text-center lg:text-xl xl:text-4xl text-xl mt-3">
         Inspiration for your next adventure
       </div>
-      <Display/>
+      <Display />
+      <Nft />
     </div>
   );
 }
---
+++ src/components/Nft.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Nft = () => {
+  return (
+    <div className="p-28  bg-[#A02279] grid lg:grid-cols-2 mt-8">
+      <div className="text-white  flex flex-col items-start">
+        <p className="text-3xl mt-5">Metabnb NFTs</p>
+        <p className="text-base mt-5">
+          Discover our NFT gift cards collection. Loyal customers gets amazing
+          gift cards which are traded as NFTs. These NFTs gives our cutomer
+          access to loads of our exclusive services.
+        </p>
+        <button className="p-2 text-[#A02279] bg-white rounded-xl mt-5">
+          Learn More
+        </button>
+      </div>
+      <div className="dsf">
+        <img src={} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Nft;
