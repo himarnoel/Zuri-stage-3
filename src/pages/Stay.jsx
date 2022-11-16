3996e3f29a4118eee65969d931f0ec1bc888d4a1
himarnoel <olaimarnoel@gmail.com>
2022-11-16 14:12:12 +0100

    added images"

--- src/pages/Stay.jsx
+++ src/pages/Stay.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Nav from "./../components/Nav";
 import { GiSettingsKnobs } from "react-icons/gi";
-import Card from './../components/Card';
+import Card from "./../components/Card";
 import img1 from "../assets/stay/1.png";
 import img2 from "../assets/stay/2.png";
 import img3 from "../assets/stay/3.png";
@@ -17,12 +17,13 @@ import img13 from "../assets/stay/13.png";
 import img14 from "../assets/stay/14.png";
 import img15 from "../assets/stay/15.png";
 import img16 from "../assets/stay/16.png";
+import Footer from "./../components/Footer";
 const Stay = () => {
   return (
     <div>
       <Nav />
 
-      <div className="lg:flex  lg:justify-evenly ">
+      <div className=" hidden ">
         <a href="" className="">
           Resuturant
         </a>
@@ -45,16 +46,31 @@ const Stay = () => {
           Farm
         </a>
 
-        <div className="border-2 flex">
+        <div className="border-2 flex ">
           <a>Location</a>
           <GiSettingsKnobs />
         </div>
-
       </div>
 
-      <div className="grid lg:grid-cols-4 md:grid-col-2 ">
-        <Card />
+      <div className="grid lg:grid-cols-4 md:grid-col-2 mb-5 ">
+        <Card img={img1} />
+        <Card img={img2} />
+        <Card img={img3} />
+        <Card img={img4} />
+        <Card img={img5} />
+        <Card img={img6} />
+        <Card img={img7} />
+        <Card img={img8} />
+        <Card img={img8} />
+        <Card img={img10} />
+        <Card img={img11} />
+        <Card img={img12} />
+        <Card img={img13} />
+        <Card img={img14} />
+        <Card img={img15} />
+        <Card img={img16} />
       </div>
+      <Footer />
     </div>
   );
 };
