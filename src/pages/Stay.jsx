1e9a45b456aa2a0f683febd4be73d68d630668cf
himarnoel <olaimarnoel@gmail.com>
2022-11-16 14:02:11 +0100

    changed location"

--- src/pages/Stay.jsx
+++ src/pages/Stay.jsx
@@ -2,6 +2,21 @@ import React from "react";
 import Nav from "./../components/Nav";
 import { GiSettingsKnobs } from "react-icons/gi";
 import Card from './../components/Card';
+import img1 from "../assets/stay/1.png";
+import img2 from "../assets/stay/2.png";
+import img3 from "../assets/stay/3.png";
+import img4 from "../assets/stay/4.png";
+import img5 from "../assets/stay/5.png";
+import img6 from "../assets/stay/6.png";
+import img7 from "../assets/stay/7.png";
+import img8 from "../assets/stay/8.png";
+import img10 from "../assets/stay/10.png";
+import img11 from "../assets/stay/11.png";
+import img12 from "../assets/stay/12.png";
+import img13 from "../assets/stay/13.png";
+import img14 from "../assets/stay/14.png";
+import img15 from "../assets/stay/15.png";
+import img16 from "../assets/stay/16.png";
 const Stay = () => {
   return (
     <div>
